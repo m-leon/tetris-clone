fe8039e518688f469673060502c132e9298f6c3b
Max Leon <max_leon@gmx.com>
2012-04-16 09:42:34 -0700

    - Removes minlog jar and replaces and adds the source - Adds close functions to Core

--- TetrisClone/src/org/mleon/tetris/Core.java
+++ TetrisClone/src/org/mleon/tetris/Core.java
@@ -1,21 +1,41 @@
 package org.mleon.tetris;
 
 import org.mleon.tetris.states.*;
+import org.mleon.tetris.util.Log;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-import com.esotericsoftware.minlog.Log;
-
 public class Core extends StateBasedGame {
+    public static int EXIT_UNKNOWN_ERROR    = -0x01;
+    public static int EXIT_NO_ERROR         = 0x00;
+    public static int EXIT_CLOSE_REQUESTED  = 0x01;
+    public static int EXIT_CONTAINER_FAIL   = 0x02;
+
     public Core() {
         super("TetrisClone");
     }
 
     public void initStatesList(GameContainer gc) throws SlickException {
         addState(new PlayState());
+    }
+
+    public static void prepClose(int status) {
+        if (status == EXIT_NO_ERROR || status == EXIT_CLOSE_REQUESTED)
+            Log.info("Closing without error");
+        else
+            Log.error("Closing with error status: " + status);
+    }
+
+    public static void close() {
+        close(EXIT_NO_ERROR);
+    }
+
+    public static void close(int status) {
+        prepClose(status);
+        System.exit(status);
     }
 
     public static void main(String[] args) {
@@ -32,7 +52,8 @@ public class Core extends StateBasedGame {
             container.setShowFPS(fpsCounter);
             container.start();
         } catch (SlickException e) {
-            Log.error("Unable to start application", e);
+            Log.error(e);
+            close(EXIT_CONTAINER_FAIL);
         }
     }
 }
