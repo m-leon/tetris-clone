a75bb9b8dfffaf1c53761797b8117dc406c3ab6f
Max Leon <max_leon@gmx.com>
2012-04-16 10:47:24 -0700

    - Adds bg.png as background image - Draws background image properly

--- TetrisClone/src/org/mleon/tetris/states/PlayState.java
+++ TetrisClone/src/org/mleon/tetris/states/PlayState.java
@@ -1,7 +1,9 @@
 package org.mleon.tetris.states;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -9,16 +11,45 @@ import org.newdawn.slick.state.StateBasedGame;
 public class PlayState extends BasicGameState {
     public static final int ID = 1;
 
+    private Image bgImage;
+    private int boardX, boardY;
+    private int boardWidth, boardHeight;
+
+    public int getID() {
+        return ID;
+    }
+
     public void init(GameContainer gc, StateBasedGame game) throws SlickException {
+        gc.getGraphics().setBackground(Color.white);
+        bgImage = new Image("./data/bg.png");
+        bgImage = createBgImage(bgImage, gc.getWidth(), gc.getHeight());
+        boardWidth = 300; // TODO: Base on resolution
+        boardHeight = 600;
+        boardX = (gc.getWidth() - boardWidth) / 2;
+        boardY = 0;
     }
 
     public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+        g.drawImage(bgImage, 0, 0);
+        g.setColor(new Color(0f, 0f, 0f, 0.5f));
+        g.fillRect(boardX, boardY, boardWidth, boardHeight);
     }
 
     public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
     }
 
-    public int getID() {
-        return ID;
+    private Image createBgImage(Image img, int width, int height) throws SlickException {
+        Image returnValue = new Image(width, height);
+        int widthCount = width / bgImage.getWidth();
+        int heightCount = height / bgImage.getHeight();
+        int imageWidth = bgImage.getWidth();
+        int imageHeight = bgImage.getHeight();
+        Graphics g = returnValue.getGraphics();
+        for (int i = 0; i <= heightCount; i++)
+            for (int j = 0; j <= widthCount; j++)
+                g.drawImage(bgImage, imageWidth * j, imageHeight * i);
+
+        g.flush();
+        return returnValue;
     }
 }
